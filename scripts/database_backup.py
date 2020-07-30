d122ce3b1102731211b4a4a30d599f67f10d11e1
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2020-07-30 12:56:34 +0200

    Improve database backup script

--- scripts/database_backup.py
+++ scripts/database_backup.py
@@ -4,34 +4,54 @@ Requires DB_AUTH environment variable and database and collection names as argum
 """
 import sys
 import json
-import zipfile
-import os.path
-sys.path.append(os.path.abspath(os.path.pardir))
-from core_lib.database.database import Database
+import os
+from pymongo import MongoClient
 
 database_name = sys.argv[1]
 collection_name = sys.argv[2]
+output_directory = sys.argv[3]
 
+
+db_host = os.environ.get('DB_HOST', 'localhost')
+db_port = os.environ.get('DB_PORT', 27017)
+db_auth = os.environ.get('DB_AUTH', None)
+username = None
+password = None
+if db_auth:
+    with open(db_auth) as json_file:
+        credentials = json.load(json_file)
+
+    username = credentials['username']
+    password = credentials['password']
+
+if username and password:
+    client = MongoClient(db_host,
+                         db_port,
+                         username=username,
+                         password=password,
+                         authSource='admin',
+                         authMechanism='SCRAM-SHA-256')
+else:
+    client = MongoClient(db_host, db_port)
+
+collection = client[database_name][collection_name]
 print('Database %s, collection %s' % (database_name, collection_name))
-Database.set_database_name(database_name)
-database = Database(collection_name)
-doc_count = database.get_count()
+doc_count = collection.count_documents({})
 print('Found %s documents' % (doc_count))
 documents = [{}]
 page = 0
-archive_name = f'{database_name}_{collection_name}.zip'
+limit = 100
+collection_path = f'{output_directory}/{collection_name}'
+os.makedirs(collection_path)
+while documents:
+    documents = collection.find({}).sort('_id', 1).skip(page * limit).limit(limit)
+    documents = [d for d in documents]
+    if not documents:
+        break
 
-with zipfile.ZipFile(archive_name, 'w', zipfile.ZIP_DEFLATED) as zip_object:
-    while documents:
-        documents = database.query(page=page, limit=100)
-        if not documents:
-            break
+    file_name = f'{collection_path}/{database_name}_{collection_name}_{page}.json'
+    with open(file_name, 'w') as output_file:
+        json.dump(documents, output_file)
 
-        file_name = f'{database_name}_{collection_name}_{page}.json'
-        with open(file_name, 'w') as output_file:
-            json.dump(documents, output_file)
-
-        zip_object.write(file_name, file_name)
-        os.remove(file_name)
-        print('Page %s done' % (page))
-        page += 1
+    print('Page %s done' % (page))
+    page += 1
