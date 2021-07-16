65783d523852b6f230c73109064c01923ea314fe
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2021-07-16 12:51:27 +0200

    Refactor requests multiple edit and add size/event and time/event

--- vue_frontend/src/components/RequestsMultipleEdit.vue
+++ vue_frontend/src/components/RequestsMultipleEdit.vue
@@ -7,41 +7,41 @@
         <li v-for="prepid in prepids" :key="prepid"><a :href="'requests?prepid=' + prepid" title="Open request in new tab" target="_blank">{{prepid}}</a></li>
       </ul>
       <h2>Values to be updated in {{prepids.length}} requests</h2>
-      <table v-if="editableRequest">
+      <table v-if="editingObject">
         <tr>
           <td>Energy</td>
-          <td><input type="number" v-model="editableRequest.energy">TeV</td>
+          <td><input type="number" v-model="editingObject.energy">TeV</td>
         </tr>
         <tr>
           <td>CMSSW Version</td>
-          <td><input type="text" v-model="editableRequest.cmssw_release"></td>
+          <td><input type="text" v-model="editingObject.cmssw_release"></td>
         </tr>
         <tr>
           <td>Lumisections</td>
           <td>
-            <JSONField v-model="editableRequest.lumisections"/>
+            <JSONField v-model="editingObject.lumisections"/>
           </td>
         </tr>
         <tr>
           <td>Memory</td>
-          <td><input type="number" v-model="editableRequest.memory">MB</td>
+          <td><input type="number" v-model="editingObject.memory">MB</td>
         </tr>
         <tr>
           <td>Notes</td>
-          <td><textarea v-model="editableRequest.notes"></textarea></td>
+          <td><textarea v-model="editingObject.notes"></textarea></td>
         </tr>
         <tr>
           <td>Priority</td>
-          <td><input type="number" v-model="editableRequest.priority"></td>
+          <td><input type="number" v-model="editingObject.priority"></td>
         </tr>
         <tr>
-          <td>Runs ({{runListLength(editableRequest.runs)}})</td>
-          <td><textarea v-model="editableRequest.runs"></textarea></td>
+          <td>Runs ({{cleanSplit(editingObject.runs).length}})</td>
+          <td><textarea v-model="editingObject.runs"></textarea></td>
         </tr>
         <tr>
-          <td>Sequences ({{listLength(editableRequest.sequences)}})</td>
+          <td>Sequences ({{listLength(editingObject.sequences)}})</td>
           <td>
-            <div v-for="(sequence, index) in editableRequest.sequences" :key="index">
+            <div v-for="(sequence, index) in editingObject.sequences" :key="index">
               <h3>Sequence {{index + 1}}</h3>
               <table v-if="!sequence.deleted">
                 <tr>
@@ -87,45 +87,78 @@
                      class="mr-1 mb-1"
                      color="error"
                      v-if="!sequence.deleted"
-                     @click="deleteSequence(index)">Delete sequence {{index + 1}}</v-btn>
+                     @click="sequence.deleted = true">Delete sequence {{index + 1}}</v-btn>
               <hr>
             </div>
           </td>
         </tr>
         <tr>
           <td>Size per event</td>
-          <td><input type="number" v-model="editableRequest.size_per_event">kB</td>
+          <td>
+            <div v-for="(sizePerEvent, sizePerEventIndex) in editingObject.size_per_event" :key="sizePerEventIndex" >
+              <input type="number"
+                     style="margin-top: 2px"
+                     v-model="editingObject.size_per_event[sizePerEventIndex]">kB
+            </div>
+          </td>
         </tr>
         <tr>
           <td>Time per event</td>
-          <td><input type="number" v-model="editableRequest.time_per_event">s</td>
+          <td>
+            <div v-for="(timePerEvent, timePerEventIndex) in editingObject.time_per_event" :key="timePerEventIndex" >
+              <input type="number"
+                     style="margin-top: 2px"
+                     v-model="editingObject.time_per_event[timePerEventIndex]">s
+            </div>
+          </td>
         </tr>
       </table>
       <h2>List of edits that will be done</h2>
       <ul>
-        <div v-for="(value, key) in editableRequest" :key="key">
-          <template v-if="key != 'sequences' && value != orgEditableRequest[key]">
+        <div v-for="(value, key) in differences" :key="key">
+          <template v-if="!['sequences', 'size_per_event', 'time_per_event'].includes(key)">
             <li>
               <b>{{key}}</b> will be set to <b>{{value}}</b> 
-              <v-btn small class="ml-1 mb-1" style="height: 22px" color="error" @click="deleteAttribute(editableRequest, key)">Remove</v-btn>
+              <v-btn small
+                     class="ml-1 mb-1"
+                     color="error"
+                     @click="editingObject[key] = originalEditingObject[key]">Remove edit</v-btn>
             </li>
           </template>
-        </div>
-        <div v-for="(sequence, index) in editableRequest.sequences" :key="index">
-          <template v-if="!sequence.deleted">
-            <div v-for="(value, key) in sequence" :key="key">
-              <template v-if="key != 'deleted' && value != orgEditableRequest.sequences[index][key]">
+          <template v-if="key == 'sequences'">
+            <div v-for="(sequence, index) in value" :key="index">
+              <template v-if="!sequence.deleted">
+                <div v-for="(sequenceValue, sequenceKey) in sequence" :key="sequenceKey">
+                  <template v-if="sequenceKey != 'deleted'">
+                    <li>
+                      <b>{{sequenceKey}}</b> in <b>Sequence {{index + 1}}</b> will be set to <b>{{sequenceValue}}</b>
+                      <v-btn small
+                             class="ml-1 mb-1"
+                             color="error"
+                             @click="editingObject.sequences[index][sequenceKey] = originalEditingObject.sequences[index][sequenceKey]">Remove edit</v-btn>
+                    </li>
+                  </template>
+                </div>
+              </template>
+              <li v-if="sequence.deleted">
+                Sequence {{index + 1}} will be <b>deleted in all {{prepids.length}}</b> requests
+                <v-btn small class="ml-1 mb-1" color="error" @click="editingObject.sequences[index].deleted = false">Remove edit</v-btn>
+              </li>
+            </div>
+          </template>
+          <template v-if="['size_per_event', 'time_per_event'].includes(key)">
+            <div v-for="(listItem, index) in value" :key="index">
+              <template v-if="listItem != null">
                 <li>
-                  <b>{{key}}</b> in <b>Sequence {{index + 1}}</b> will be set to <b>{{value}}</b>
-                  <v-btn small class="ml-1 mb-1" style="height: 22px" color="error" @click="deleteAttribute(sequence, key)">Remove</v-btn>
+                  <b>{{key}}</b> value at <b>position {{index + 1}}</b> will be set to <b>{{listItem}}</b>
+                  <v-btn small
+                         class="ml-1 mb-1"
+                         color="error"
+                         @click="$set(editingObject[key], index, originalEditingObject[key][index])">Remove edit</v-btn>
                 </li>
               </template>
             </div>
           </template>
-          <li v-if="sequence.deleted">
-            Sequence {{index + 1}} will be <b>deleted in all {{prepids.length}}</b> requests
-            <v-btn small class="ml-1 mb-1" style="height: 22px" color="error" @click="undeleteSequence(index)">Bring back sequence {{index + 1}}</v-btn>
-          </li>
         </div>
       </ul>
       <v-btn small class="mr-1 mt-1" color="primary" @click="save()">Save</v-btn>
@@ -170,9 +203,10 @@ export default {
   data () {
     return {
       prepids: [],
-      requests: [],
-      editableRequest: {},
-      orgEditableRequest: {},
+      objects: [],
+      editingObject: {},
+      originalEditingObject: {},
+      differences: {},
       loading: true,
       errorDialog: {
         visible: false,
@@ -187,114 +221,167 @@ export default {
     this.prepids = this.cleanSplit(query['prepid']);
     let component = this;
     axios.get('api/requests/get_editable/' + this.prepids.join(',')).then(response => {
-      let requests = response.data.response.object;
+      let objects = response.data.response.object;
       if (component.prepids.length == 1) {
-        requests = [requests];
+        objects = [objects];
       }
-      for (let request of requests) {
-        request.runs = request.runs.join('\n');
-        request.lumisections = component.stringifyLumis(request.lumisections);
-        for (let sequence of request.sequences) {
-          sequence.datatier = sequence.datatier.join(',');
-          sequence.eventcontent = sequence.eventcontent.join(',');
-          sequence.step = sequence.step.join(',');
-        }
+      for (let obj of objects) {
+        this.prepareObjectForEditing(obj);
       }
-      component.requests = requests;
-      let placeholders = component.makePlaceholders(requests);
-      component.$set(component, 'editableRequest', placeholders);
-      component.$set(component, 'orgEditableRequest', component.makeCopy(placeholders))
+      component.objects = objects;
+      let editingObject = component.makeEditingObject(objects);
+      component.$set(component, 'editingObject', component.makeCopy(editingObject));
+      component.$set(component, 'originalEditingObject', component.makeCopy(editingObject))
       component.loading = false;
     }).catch(error => {
       component.loading = false;
       component.showError('Error getting request information', component.getError(error));
     });
   },
-  methods: {
-    makePlaceholders: function(requests) {
-      let placeholders = {};
-      for (let key of ['energy', 'cmssw_release', 'lumisections', 'memory', 'notes',
-                       'priority', 'runs', 'time_per_event', 'size_per_event']) {
-        let sameValue = true;
-        for (let i = 0; i < requests.length - 1; i++) {
-          if (JSON.stringify(requests[i][key]) != JSON.stringify(requests[i + 1][key])) {
-            sameValue = false;
-            break
-          }
-        }
-        placeholders[key] = sameValue ? this.makeCopy(requests[0][key]) : '';
-      }
-      let minSequences = Math.min(...requests.map(x => x.sequences.length));
-      placeholders.sequences = [];
-      for (let i = 0; i < minSequences; i++) {
-        placeholders.sequences.push({'deleted': false});
-      }
-      for (let key of ['conditions', 'customise', 'datatier', 'era', 'eventcontent',
-                       'extra', 'nThreads', 'scenario', 'step']) {
-        for (let sequenceIndex = 0; sequenceIndex < minSequences; sequenceIndex++) {
-          let sameValue = true;
-          for (let i = 0; i < requests.length - 1; i++) {
-            if (JSON.stringify(requests[i].sequences[sequenceIndex][key]) != JSON.stringify(requests[i + 1].sequences[sequenceIndex][key])) {
-              sameValue = false;
-              break
-            }
-          }
-          placeholders.sequences[sequenceIndex][key] = sameValue ? this.makeCopy(requests[0].sequences[sequenceIndex][key]) : '';
-        }
-      } 
-      return placeholders;
-    },
-    save: function() {
-      let requests = this.makeCopy(this.requests);
-      let component = this;
-      // First - update request attributes
-      for (let key in this.editableRequest) {
-        if (key == 'sequences') {
-          continue;
-        }
-        const value = this.editableRequest[key];
-        if (value != this.orgEditableRequest[key]) {
-          for (let request of requests) {
-            request[key] = value;
-          }
-        }
-      }
-      // Then - update request sequences attributes
-      for (let sequenceIndex in this.editableRequest.sequences) {
-        const sequence = this.editableRequest.sequences[sequenceIndex];
-        for (let request of requests) {
-          const requestSequences = request.sequences;
-          if (sequenceIndex >= requestSequences.length) {
-            continue;
-          }
-          let requestSequence = requestSequences[sequenceIndex];
-          if (sequence.deleted) {
-            // Removal from list will happen later
-            requestSequences[sequenceIndex] = undefined;
+  watch: {
+    editingObject: {
+      deep: true,
+      handler() {
+        let differences = {};
+        let ref = this.originalEditingObject;
+        let tar = this.editingObject;
+        for (let key of Object.keys(ref)) {
+          if (['sequences', 'time_per_event', 'size_per_event'].includes(key)) {
             continue
           }
-          // Sequence attributes
-          for (let key in sequence) {
-            if (key == 'deleted') {
-              continue
-            }
-            requestSequence[key] = sequence[key];
+          if (ref[key] != tar[key]) {
+            differences[key] = tar[key]
           }
         }
+        // Sequence diffs
+        let sequencesDiffs = [];
+        for (let i = 0; i < ref.sequences.length; i++) {
+          let refSeq = ref.sequences[i];
+          let tarSeq = tar.sequences[i];
+          let seqDiff = {'deleted': tarSeq.deleted};
+          sequencesDiffs.push(seqDiff);
+          for (let key of Object.keys(refSeq)) {
+            if (refSeq[key] != tarSeq[key]) {
+              if (key == 'nThreads') {
+                seqDiff[key] = parseInt(tarSeq[key]);
+              } else {
+                seqDiff[key] = tarSeq[key];
+              }
+            }
+          }
+        }
+        if (sequencesDiffs.some(s => s.deleted || Object.keys(s).length > 1)) {
+          differences['sequences'] = sequencesDiffs;
+        }
+        // Size per event and time per event diffs
+        let sizePerEventDiffs = Array(ref.sequences.length);
+        let timePerEventDiffs = Array(ref.sequences.length);
+        for (let i = 0; i < ref.sequences.length; i++) {
+          sizePerEventDiffs[i] = ref.size_per_event[i] != tar.size_per_event[i] ? parseFloat(tar.size_per_event[i]) : null;
+          timePerEventDiffs[i] = ref.time_per_event[i] != tar.time_per_event[i] ? parseFloat(tar.time_per_event[i]) : null;
+        }
+        if (sizePerEventDiffs.some(s => s !== null)) {
+          differences['size_per_event'] = sizePerEventDiffs;
+        }
+        if (timePerEventDiffs.some(s => s !== null)) {
+          differences['time_per_event'] = timePerEventDiffs;
+        }
+        this.$set(this, 'differences', differences);
       }
-      for (let request of requests) {
-        request.sequences = request.sequences.filter(s => s !== undefined);
-        request.notes = request.notes.trim();
-        request.runs = this.cleanSplit(request.runs);
-        request.lumisections = request.lumisections ? JSON.parse(request.lumisections) : {};
-        for (let sequence of request.sequences) {
-          sequence.datatier = this.cleanSplit(sequence.datatier);
-          sequence.eventcontent = this.cleanSplit(sequence.eventcontent);
-          sequence.step = this.cleanSplit(sequence.step);
+    }
+  },
+  methods: {
+    prepareObjectForEditing: function(obj) {
+      obj.runs = obj.runs.join('\n');
+      obj.lumisections = this.stringifyLumis(obj.lumisections);
+      for (let sequence of obj.sequences) {
+        sequence.datatier = sequence.datatier.join(',');
+        sequence.eventcontent = sequence.eventcontent.join(',');
+        sequence.step = sequence.step.join(',');
+      }
+    },
+    prepareObjectForSaving: function(obj) {
+      obj.notes = obj.notes.trim();
+      obj.runs = this.cleanSplit(obj.runs);
+      obj.lumisections = obj.lumisections ? JSON.parse(obj.lumisections) : {};
+      for (let sequence of obj.sequences) {
+        sequence.datatier = this.cleanSplit(sequence.datatier);
+        sequence.eventcontent = this.cleanSplit(sequence.eventcontent);
+        sequence.step = this.cleanSplit(sequence.step);
+      }
+    },
+    jsonDiff: function(obj1, obj2) {
+      return JSON.stringify(obj1) != JSON.stringify(obj2);
+    },
+    getCommonValue: function(objects, callback) {
+      for (let i = 0; i < objects.length - 1; i++) {
+        if (this.jsonDiff(callback(objects[i]), callback(objects[i + 1]))) {
+          return null;
         }
       }
+      return callback(objects[0]);
+    },
+    makeEditingObject: function(requests) {
+      let editingObject = {};
+      // Primitive attributes
+      for (let key of ['energy', 'cmssw_release', 'lumisections', 'memory', 'notes', 'priority', 'runs']) {
+        editingObject[key] = this.getCommonValue(requests, (r) => r[key]);
+      }
+      // Sequences
+      editingObject.sequences = Array(Math.min(...requests.map(x => x.sequences.length)));
+      for (let i = 0; i < editingObject.sequences.length; i++) {
+        editingObject.sequences[i] = {'deleted': false};
+      }
+      // Sequence attributes
+      for (let key of Object.keys(requests[0].sequences[0])) {
+        for (let i = 0; i < editingObject.sequences.length; i++) {
+          editingObject.sequences[i][key] = this.getCommonValue(requests, (r) => r.sequences[i][key]);
+        }
+      }
+      // Time per event and size per event
+      editingObject.size_per_event = Array(editingObject.sequences.length);
+      editingObject.time_per_event = Array(editingObject.sequences.length);
+      for (let i = 0; i < editingObject.sequences.length; i++) {
+        editingObject.size_per_event[i] = this.getCommonValue(requests, (r) => r.size_per_event[i]);
+        editingObject.time_per_event[i] = this.getCommonValue(requests, (r) => r.time_per_event[i]);
+      }
+      return editingObject;
+    },
+    save: function() {
+      let objects = this.makeCopy(this.objects);
+      let component = this;
+      for (let obj of objects) {
+        for (let key of Object.keys(this.differences)) {
+          let difference = this.differences[key];
+          if (key == 'sequences') {
+            for (let seqIndex = 0; seqIndex < difference.length; seqIndex++) {
+              let diffSeq = difference[seqIndex];
+              if (diffSeq.deleted) {
+                obj.sequences[seqIndex] = undefined;
+              } else {
+                let objSeq = obj.sequences[seqIndex];
+                for (let seqKey of Object.keys(diffSeq)) {
+                  if (seqKey != 'deleted') {
+                    objSeq[seqKey] = diffSeq[seqKey];
+                  }
+                }
+              }
+            }
+            obj.sequences = obj.sequences.filter(s => s !== undefined);
+          } else if (['size_per_event', 'time_per_event'].includes(key)) {
+            for (let i = 0; i < difference.length; i++) {
+              if (difference[i] != null) {
+                obj[key][i] = difference[i];
+              }
+            }
+          } else {
+            obj[key] = difference;
+          }
+        }
+        this.prepareObjectForSaving(obj);
+      }
       this.loading = true;
-      let httpRequest = axios.post('api/requests/update', requests)
+      let httpRequest = axios.post('api/requests/update', objects)
       httpRequest.then(response => {
         component.loading = false;
         window.location = 'requests?prepid=' + response.data.response.map(x => x.prepid).join(',');
@@ -316,23 +403,7 @@ export default {
       this.errorDialog.title = title;
       this.errorDialog.description = description;
       this.errorDialog.visible = true;
-    },
-    setValue: function(obj, key, value) {
-      this.$set(obj, key, value);
-    },
-    deleteAttribute: function(obj, name) {
-      obj[name] = undefined;
-      delete obj[name];
-    },
-    deleteSequence: function(sequenceIndex) {
-      this.editableRequest.sequences[sequenceIndex].deleted = true;
-    },
-    undeleteSequence: function(sequenceIndex) {
-      this.editableRequest.sequences[sequenceIndex].deleted = false;
-    },
-    runListLength: function(list) {
-      return this.cleanSplit(list).length;
-    },
+    }
   }
 }
 </script>
